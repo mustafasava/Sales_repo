4d7b7ea147a3948a515eceb14a737f2ad0b70127
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-23 16:55:13 +0300

    nn

---
+++ modules/prep_sofico.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, text
+
+def sofico_prep(x,y,month=None,year=None):
+    if y == 1:
+        try:
+            engine = create_engine("sqlite:///.\\DBs\\dist_native\\dist_native.db")
+
+            with open(".\\DBs\\dist_native\\queries\\sofico_prep.sql", "r", encoding="utf-8") as f:
+                sql_query = f.read()
+
+            with engine.begin() as conn:
+                conn.execute(text("DELETE FROM prep_sofico Where Month = :month AND Year = :year"),{"month": month, "year": year})
+                conn.execute(text("PRAGMA incremental_vacuum;"))
+                conn.execute(text(sql_query),{"month": month, "year": year})
+                conn.execute(text("PRAGMA incremental_vacuum;"))
+                x = f" {x} Data Prepared Successfully."
+                return x,y
+        except Exception as e:
+            x = f"\n Error happened while preparing :\n Details : {e}"
+            y=0
+            return x,y
+    else:
+        return x,y
