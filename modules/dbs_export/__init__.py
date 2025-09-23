66201b4223b8a728d4702702fb1df9b54a0f9f05
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-23 16:33:23 +0300

    all

--- modules/dbs_export/__init__.py
+++
@@ -1,7 +0,0 @@
-from .POS_exp import pos_exp
-from .IBS_exp import ibs_exp
-from .Sofico_exp import sofico_exp
-from .EPDA_exp import epda_exp
-from .EgyDrug_exp import egydrug_exp
-
-__all__ = ["pos_exp", "ibs_exp", "sofico_exp", "epda_exp", "egydrug_exp"]
--- modules/preparing/__init__.py
+++
@@ -1,7 +0,0 @@
-from .IBS_prep import ibs_prep
-from .POS_prep import pos_prep
-from .Sofico_prep import sofico_prep
-from .EPDA_prep import epda_prep
-from .EgyDrug_sales_prep import egydrug_sales_prep
-
-__all__ = ["ibs_prep", "pos_prep", "sofico_prep", "epda_prep", "egydrug_sales_prep"]
