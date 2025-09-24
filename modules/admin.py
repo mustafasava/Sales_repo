ac5430e69fb85b3c209d38bdfa8bfdc9d4a11421
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 16:50:04 +0300

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -7,5 +7,8 @@ from info import dist_list
 def admin():
 
     result = upload(st.file_uploader("Upload Excel file", type=["xlsx", "xls"]))
-    dist_list[result[1]](result[0], result[1], result[2], result[3])
-    
+    if result[1] == None:
+        ""
+    else:
+        dist_list[result[1]](result[0], result[1], result[2], result[3])
+
