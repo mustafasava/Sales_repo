6b1812b8185740c7e27d2ee3f1fd5ff721701d92
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 17:25:49 +0300

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -10,7 +10,7 @@ def admin():
 
     if uploaded is not None:
         
-        dist_list([uploaded[1]](uploaded[0], uploaded[1], uploaded[2], uploaded[3]))
+        cleaned = dist_list[uploaded[1]](uploaded[0], uploaded[1], uploaded[2], uploaded[3])
         
 
     
