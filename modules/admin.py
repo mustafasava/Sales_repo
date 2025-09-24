1748b28d4d832e4dd50e838badd64555156533a2
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 16:56:49 +0300

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -7,5 +7,6 @@ from info import dist_list
 def admin():
 
     result = upload(st.file_uploader("Upload Excel file", type=["xlsx", "xls"]))
-    dist_list[result[1]](result[0], result[1], result[2], result[3])
+    uploaded_file, distname, year, month = result
+    dist_list[distname](uploaded_file, distname, year, month)
 
