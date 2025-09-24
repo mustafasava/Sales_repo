bcd983d4a1a7fac91bcec4978c560668efacc5bb
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 13:13:59 +0300

    n

--- modules/auth.py
+++ modules/auth.py
@@ -1,11 +1,7 @@
 import streamlit as st
 
-# Hardcoded users: name: [role, password, area]
-auth = {
-    "user1": ["admin", "1234", "all"],
-    "user2": ["sales", "1234", "ALEX"],
-    "user3": ["sales", "1234", "Cairo"]
-}
+
+from info import  auth
 
 
 def login():
---
+++ modules/info.py
@@ -0,0 +1,6 @@
+dist_list = ["ibs","pos","epda","egydrug","sofico"]
+
+auth = {"user1": ["admin", "1234", "all"],
+        "user2": ["sales", "1234", "alex"],
+        "user3": ["sales", "1234", "cairo"]}
+
