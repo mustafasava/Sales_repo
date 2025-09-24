fa6bda98fe8ce24ddf3dfcff73e1c73734ba7a8e
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 13:40:46 +0300

    Update upload.py

--- modules/upload.py
+++ modules/upload.py
@@ -13,4 +13,4 @@ def upload():
 def validate(filename):
     pattern = r"^([a-zA-Z]+)_(\d{4})_(0?[1-9]|1[0-2])\.(xlsx|xls)$"
     if re.match(pattern,filename) == None:
-        st.error(f"no matching")
+        st.error(f"The naming pattern is wrong please name like : distname_year_month ,\n example: ibs_2025_7")
