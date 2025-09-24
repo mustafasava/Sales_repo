cc44ceff840705cd67fdee25f31d7d9ecc4df24a
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 16:59:55 +0300

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -9,5 +9,5 @@ def admin():
     result = upload(st.file_uploader("Upload Excel file", type=["xlsx", "xls"]))
     if result is not None:
         
-        dist_list[result[1]](result[0], result[1], result[2], result[3])
+        st.write(dist_list[result[1]](result[0], result[1], result[2], result[3]))
 
