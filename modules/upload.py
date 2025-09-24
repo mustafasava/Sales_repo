cc323c56ae6ce345a4f4466e3859e3bea8fef79e
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 13:30:54 +0300

    Update upload.py

--- modules/upload.py
+++ modules/upload.py
@@ -4,8 +4,4 @@ import streamlit as st
 def upload():
     uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
 
-    if uploaded_file is not None:
-        st.success(f"File uploaded: {uploaded_file.name}")
-        st.write("File details:")
-        st.write(f"- Type: {uploaded_file.type}")
-        st.write(f"- Size: {uploaded_file.size} bytes")
+    
