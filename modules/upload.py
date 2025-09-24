a90e2e7de239b136e3436b406b861e496456a54f
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-24 15:11:53 +0300

    Update upload.py

--- modules/upload.py
+++ modules/upload.py
@@ -7,13 +7,19 @@ def upload(uploaded_file):
     if uploaded_file is not None:
         st.success(f"File uploaded: {uploaded_file.name}")
         pattern = r"^([a-zA-Z]+)_(\d{4})_(0?[1-9]|1[0-2])\.(xlsx|xls)$"
-        if re.match(pattern,uploaded_file.name) == None:
+        match = re.match(pattern,uploaded_file.name)
+        if  match != None:
+            st.success(f"Validation Done !, File is accepted.")
+            distname , year , month , ext = match.groups()
+            return distname , year , month
+        
+        else:
             st.markdown(
                         """
                         <div style="background-color:#f8d7da; color:#721c24; padding:10px; border-radius:5px; border:1px solid #f5c6cb;">
                             ❌ The naming pattern is wrong.<br>
-                            Please name like: distname_year_month<br>
-                            <h6>Example</h6>: ibs_2025_7
+                            Please name like: distname_year_month  ,<br>
+                            Example   : ibs_2025_7
                         </div>
                         """,
                         unsafe_allow_html=True
@@ -21,5 +27,6 @@ def upload(uploaded_file):
 
 
 
-        else:
-            st.success(f"Validation Done !, File is accepted.")
+        
+            
+            
