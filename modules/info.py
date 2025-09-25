f9ecdd65f1e0ab59a4280053b4a4b2573cf9dd44
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-25 15:34:08 +0300

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,3 +1,22 @@
+from cln_ibs import cln_ibs
+from prep_ibs import prep_ibs
+from cln_pos import cln_pos
+from prep_pos import prep_pos
+from cln_sofico import cln_sofico
+from prep_sofico import prep_sofico
+from cln_epda import cln_epda
+from prep_epda import prep_epda
+from cln_egydrug import cln_egydrug
+from prep_egydrug import prep_egydrug
+
+
+dist_list = {
+    "ibs": [cln_ibs, prep_ibs],
+    "pos": [cln_pos, prep_pos],
+    "sofico": [cln_sofico, prep_sofico],
+    "epda": [cln_epda, prep_epda],
+    "egydrug": [cln_egydrug, prep_egydrug],
+}
 
 
 dist_list = {
