9b289cef91a153a4116de5ae8c16003441430642
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-25 16:16:49 +0300

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -13,10 +13,10 @@ def admin():
         cleaned = dist_list[uploaded[1]][0](uploaded[0], uploaded[1], uploaded[2], uploaded[3])
         save(cleaned[0], cleaned[1], cleaned[2], cleaned[3],"cleaned")
         
-        if cleaned is not None:
+        # if cleaned is not None:
             
-            prepared = dist_list[cleaned[1]][1](cleaned[0], cleaned[1], cleaned[2], cleaned[3])
-            save(prepared[0], prepared[1], prepared[2], prepared[3],"prepared")
-            st.success(f"( {prepared[1]} ) sheet has been uploaded, cleaned, prepared and saved successfully !")
+        #     prepared = dist_list[cleaned[1]][1](cleaned[0], cleaned[1], cleaned[2], cleaned[3])
+        #     save(prepared[0], prepared[1], prepared[2], prepared[3],"prepared")
+        #     st.success(f"( {prepared[1]} ) sheet has been uploaded, cleaned, prepared and saved successfully !")
     
 
