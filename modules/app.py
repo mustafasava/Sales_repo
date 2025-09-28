3e3595350b682ea1b85bc017cec887fe066ee660
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-28 17:15:41 +0300

    Update app.py

--- modules/app.py
+++ modules/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from auth import login, logout
 from admin import admin
+from info import dist_list
 # ------------------- LOGIN -------------------
 if not login():
     st.stop()
@@ -12,5 +13,11 @@ if st.sidebar.button("Logout"):
 if st.session_state.role == "admin":
     try:
         admin()
+        distname = st.selectbox("Select Distributor", dist_list)
+
+        # 2️⃣ Button to trigger the process
+        if st.button("Run Mapping Process"):
+            if not distname:
+                st.error("Please select a distributor first.")
     except Exception as e:
         pass
