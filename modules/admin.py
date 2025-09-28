2e0492f1feedaca0ec5e5e5fb8c343d95674e5a2
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-28 15:58:45 +0300

    Update admin.py

--- modules/admin.py
+++ modules/admin.py
@@ -6,17 +6,19 @@ from save import save
 
 def admin():
 
-    uploaded = upload(st.file_uploader("Upload Excel file", type=["xlsx", "xls"]))
+    try:
+        uploaded = upload(st.file_uploader("Upload Excel file", type=["xlsx", "xls"]))
 
-    if uploaded is not None:
-        
-        cleaned = dist_list[uploaded[1]][0](uploaded[0], uploaded[1], uploaded[2], uploaded[3])
-        save(cleaned[0], cleaned[1], cleaned[2], cleaned[3],"cleaned")
-        
-        # if cleaned is not None:
+        if uploaded is not None:
             
-        #     prepared = dist_list[cleaned[1]][1](cleaned[0], cleaned[1], cleaned[2], cleaned[3])
-        #     save(prepared[0], prepared[1], prepared[2], prepared[3],"prepared")
-        #     st.success(f"( {prepared[1]} ) sheet has been uploaded, cleaned, prepared and saved successfully !")
-    
-
+            cleaned = dist_list[uploaded[1]][0](uploaded[0], uploaded[1], uploaded[2], uploaded[3])
+            save(cleaned[0], cleaned[1], cleaned[2], cleaned[3],"cleaned")
+            
+            # if cleaned is not None:
+                
+            #     prepared = dist_list[cleaned[1]][1](cleaned[0], cleaned[1], cleaned[2], cleaned[3])
+            #     save(prepared[0], prepared[1], prepared[2], prepared[3],"prepared")
+            #     st.success(f"( {prepared[1]} ) sheet has been uploaded, cleaned, prepared and saved successfully !")
+        
+    except Exception as e:
+        st.error(f"{e}")
