94d98cc0b42cb26844a93c106ed04e0a3d5f0582
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-09-29 18:46:46 +0300

    m

--- modules/mapping.py
+++ modules/mapping.py
@@ -18,7 +18,7 @@ def mapping(prep_df,dist_name,year,month):
         how="left"
     )
     
-    missed_products = merged_products[merged_products["dist_itemcode"].isna()][["item_code","item_name","dist_itemcode"]]
+    missed_products = merged_products[merged_products["dist_itemcode"].isna()][["item_code","item_name","dist_itemcode"]].drop_duplicates()
 
     if not missed_products.empty:
         download(missed_products, filename=f"missed_products_{dist_name}_{year}_{month}.xlsx")
@@ -35,7 +35,7 @@ def mapping(prep_df,dist_name,year,month):
         how="left"
     )
     
-    missed_bricks = merged_bricks[merged_bricks["dist_brickcode"].isna()][dist_list[dist_name][2]]
+    missed_bricks = merged_bricks[merged_bricks["dist_brickcode"].isna()][dist_list[dist_name][2]].drop_duplicates()
 
     if not missed_bricks.empty:
         download(missed_bricks, filename=f"missed_bricks_{dist_name}_{year}_{month}.xlsx")
@@ -55,6 +55,8 @@ def mapping(prep_df,dist_name,year,month):
             right_on="dist_brickcode",
             how="left"
         )
-        
         return final_merged , dist_name, year ,month
+    
+    else:
+        return None
         
