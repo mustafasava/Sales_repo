046297c9b257372f7febdc6691df7bf837f8af0d
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-10-01 16:45:39 +0300

    h

--- modules/prep_egydrug.py
+++ modules/prep_egydrug.py
@@ -15,7 +15,7 @@ def prep_egydrug(cleaned_file , distname , year , month):
                             
                         })
         
-
+        prepared_file["CUSTOMER_CODE"] = prepared_file["CUSTOMER_CODE"].astype(str)
         prepared_file["BRANCH_CODE"] = prepared_file["BRANCH_CODE"].astype(str)
         prepared_file["brick_code"] = np.where(
                                     prepared_file["BRANCH_CODE"].str.startswith("06"),
