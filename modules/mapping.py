bf9a82bd64148577209e5a28f76a3f958f519fdd
mustafasava <mostafa.mohammed2016@feps.edu.eg>
2025-10-02 16:15:14 +0300

    Update mapping.py

--- modules/mapping.py
+++ modules/mapping.py
@@ -24,7 +24,7 @@ def check_missing(prep_df,dist_name,year,month):
         if not missed_products.empty:
 
             products_list = pd.read_excel("./mapping/main_lists.xlsx",sheet_name="products")
-            name_to_code = dict(zip(products_list["product_name"], products_list["product_code"]))
+            name_to_code = dict(zip(products_list["product"], products_list["barcode"]))
             disabled_colsp = [col for col in missed_products.columns if col != "item"]
 
             st.write("### Enter missing Products mappings")
